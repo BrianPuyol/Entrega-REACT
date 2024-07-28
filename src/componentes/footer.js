eb15788ad5314ca81301686e2190a60b240862ce
BrianPuyol <brianpuyol1997@gmail.com>
2024-07-28 17:16:24 -0300

    final

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -3,18 +3,18 @@ const Footer = (props) => {
         <footer>
             <div className="contenedor_footer">
                 <p>
-                    <a href="">PREGUNTAS FRECUENTES</a> | <a href="">CONTACTO</a> | <a href="">BOTÓN DE ARREPENTIMIENTO</a> | <a href="">CAMBIO Y DEVOLUCIONES</a>
+                    <a href="#">PREGUNTAS FRECUENTES</a> | <a href="#">CONTACTO</a> | <a href="#">BOTÓN DE ARREPENTIMIENTO</a> | <a href="#">CAMBIO Y DEVOLUCIONES</a>
                 </p>
                 <p>
                     SEGUINOS EN NUESTRAS REDES SOCIALES
                 </p>
                 <div className="redes">
                     <img src="./imagenes/instagram.svg" alt="instagram" width="20px" />
-                    <span><a href="">TENES.FRIO?</a></span>
+                    <span><a href="#">TENES.FRIO?</a></span>
                     <img src="./imagenes/facebook-f.svg" alt="facebook" width="15px" />
-                    <span><a href="">TENES.FRÍO?</a></span>
+                    <span><a href="#">TENES.FRÍO?</a></span>
                     <img src="./imagenes/twitter.svg" alt="twitter" width="20px" />
-                    <span><a href="">TENES.FRÍO?</a></span>
+                    <span><a href="#">TENES.FRÍO?</a></span>
                 </div>
 
             </div>
--- src/componentes/header.js
+++ src/componentes/header.js
@@ -3,11 +3,11 @@ const Header = (props) => {
         <header className="holder">
             <div className="iconos">
                 <div className="icono_1">
-                    <a href=""><img src="./imagenes/bars-solid.svg" alt="barras" width="25px" /></a>
+                    <a href="#"><img src="./imagenes/bars-solid.svg" alt="barras" width="25px" /></a>
                 </div>
 
                 <div className="icono_2">
-                    <a href=""><img src="./imagenes/magnifying-glass-solid.svg" alt="busqueda" width="25px" /></a>
+                    <a href="#"><img src="./imagenes/magnifying-glass-solid.svg" alt="busqueda" width="25px" /></a>
                 </div>
 
                 <div className="icono_3">
@@ -15,11 +15,11 @@ const Header = (props) => {
                 </div>
 
                 <div className="icono_4">
-                    <a href=""><img src="./imagenes/user-solid.svg" alt="usuario" width="25px" /></a>
+                    <a href="#"><img src="./imagenes/user-solid.svg" alt="usuario" width="25px" /></a>
                 </div>
 
                 <div className="icono_5">
-                    <a href=""><img src="./imagenes/shopify.svg" alt="shopping" width="25px" /></a>
+                    <a href="#"><img src="./imagenes/shopify.svg" alt="shopping" width="25px" /></a>
                 </div>
             </div>
         </header>
